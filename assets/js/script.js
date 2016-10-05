3e0cb9e88470d7c8e70c4f9cdd14a7605394eeff
Yuri Morales <yurimorales19@gmail.com>
2016-10-04 21:26:26 -0300

    construindo cenario e fae 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,67 +1,274 @@
-var iw = 2000; // Image width
-var ih = 600; // Image height
-var sw = 570; // Canvas width
-var sh = 600; // Canvas height
-var cw = 30; // Personagem width
-var ch = 31; // Personagem height
-var speed = 10; // Character speed
+//Global variables that will be accessed in the functions below.
+var currentKey; // records the current key pressed
+var charStep = 2; // 1=1st foot, 2=stand, 3=2nd foot, 4=stand
+var charSpeed = 400; // how fast the character will move
 
-var img = new Image(iw, ih);
-var personagem = new Image(cw, ch);
+var charX = 4; // 1 unit = 32px
+var charY = 6; // 1 unit = 32px
 
-img.src = "images/paisagem.jpg";
-personagem.src = "images/guildo.png";
+var stageWidth = 640;
+var stageHeight = 480;
+var mapWidth = 800;
+var mapHeight = 800;
 
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+//Character Walk Function
+function moveChar(dir) {
 
-var pos = {x: 0, y: ih - 160};
-var camera = {x: 0, y: 0}
+	//a player could switch key mid-animation
+	//record the key that was down when animation started
+	var currentKeyCheck = currentKey;
 
-function draw() {
-	// Faz a esquerda da câmera começar meia tela antes do personagem,
-	// mas somente se tiver imagem suficiente pra isso
-	camera.x = Math.min(iw - sw, Math.max(0,pos.x - sw/2));
-	
-	// Reseta as transformações do canvas
-	ctx.setTransform(1,0,0,1,0,0);
+	//adjust from lang to code
+	if (dir == 'up') dir = 'back';
+	if (dir == 'down') dir = 'front';
+ 
+	charStep++;
+	if (charStep == 5) charStep = 1;
+ 
+	//remove the current class
+	$('#guildo').removeAttr('class');
+ 
+	//add the new class
+	switch(charStep) {
 
-	// Limpa o canvas
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	
-	// Desloca o mundo inteiro, simulando uma câmera
-	ctx.translate(-camera.x, -camera.y);
+    case 1: 
+		$('#guildo').addClass(dir+'-stand'); 
 
-	// Desenha o fundo
-	ctx.drawImage(img, 0, 0, sw, sh, 0, 0, iw, ih);
+		setTimeout(function() { 
+			charStep++;
+			if (charStep == 5) charStep = 1;
+			$('#guildo').removeAttr('class');
+			$('#guildo').addClass(dir+'-right'); 
+		}, (charSpeed/3));
 
-	// Desenha um pixel preto no cenário
-	// representando o personagem do jogo
-	//ctx.fillRect(pos.x, pos.y, cw, ch);
+		setTimeout(function() { 
+			charStep++;
+			if (charStep == 5) charStep = 1;
+			$('#guildo').removeAttr('class');
+			$('#guildo').addClass(dir+'-stand'); 
+		}, ((charSpeed/3)*2));
+		break;
 
-	//Agora sim, desenhando o personagem :)
-	ctx.drawImage(personagem, 0, 30, cw, ch, 0, 500, cw, ch);
+	case 2: 
+		$('#guildo').addClass(dir+'-right');
 
-	window.requestAnimationFrame(draw);
+		setTimeout(function() { 
+			charStep++;
+			if (charStep == 5) charStep = 1;
+			$('#guildo').removeAttr('class');
+			$('#guildo').addClass(dir+'-stand'); 
+		}, (charSpeed/3));
 
-}
+		setTimeout(function() { 
+			charStep++;
+			if (charStep == 5) charStep = 1;
+			$('#guildo').removeAttr('class');
+			$('#guildo').addClass(dir+'-left'); 
+		}, ((charSpeed/3)*2));
+		break;
 
-document.addEventListener('keydown', function(e) {
-	var key = e.which || e.key || e.keyCode;
-	switch(key) {
-		case 37:
-			console.log('left');
-			pos.x-=speed; 
+    case 3: 
+      	$('#guildo').addClass(dir+'-stand');
+
+		setTimeout(function() { 
+			charStep++;
+			if (charStep == 5) charStep = 1;
+			$('#guildo').removeAttr('class');
+			$('#guildo').addClass(dir+'-left'); 
+		}, (charSpeed/3));
+
+		setTimeout(function() { 
+			charStep++;
+			if (charStep == 5) charStep = 1;
+			$('#guildo').removeAttr('class');
+			$('#guildo').addClass(dir+'-stand'); 
+			}, ((charSpeed/3)*2)
+		); 
+    	break;
+
+    case 4: 
+		$('#guildo').addClass(dir+'-left');
+
+		setTimeout(function() { 
+			charStep++;
+			if (charStep == 5) charStep = 1;
+			$('#guildo').removeAttr('class');
+			$('#guildo').addClass(dir+'-stand'); 
+			}, (charSpeed/3)
+		);
+
+		setTimeout(function() { 
+			charStep++;
+			if (charStep == 5) charStep = 1;
+				$('#guildo').removeAttr('class');
+				$('#guildo').addClass(dir+'-right'); 
+			},((charSpeed/3)*2)
+		);
+		break;
+	}
+ 
+	//move the char
+	switch(dir) {
+
+		case 'front':
+		    if (chkMove(dir) == 'mapa') {
+		  		$('#mapa').animate({top: '-=32'}, charSpeed, "linear", function() {
+					charY++;
+					$('#top').html(charY);
+					$('#left').html(charX);
+		    		if (currentKey == currentKeyCheck) moveChar(dir);
+			  	});
+			}
+			else if (chkMove(dir) == 'guildo') {
+		  		$('#guildo').animate({top: '+=32'}, charSpeed, "linear", function() {
+		  			charY++;
+					$('#top').html(charY);
+					$('#left').html(charX);
+			    	if (currentKey == currentKeyCheck) moveChar(dir);
+			  	});
+			}
+			// $('#guildo').animate({top: '+=32'}, charSpeed, "linear", function() {
+			// 	if (currentKey == currentKeyCheck) moveChar(dir);
+			// });
 			break;
 
-		case 39:
-			console.log('right');
-			pos.x+=speed;
+		case 'back':
+			if (chkMove(dir) == 'mapa') {
+				$('#mapa').animate({top: '+=32'}, charSpeed, "linear", function() {
+					charY--;
+					$('#top').html(charY);
+					$('#left').html(charX);
+					if (currentKey == currentKeyCheck) moveChar(dir);
+				});
+			}else if (chkMove(dir) == 'guildo') {
+				$('#guildo').animate({top: '-=32'}, charSpeed, "linear", function() {
+					charY--;
+					$('#top').html(charY);
+					$('#left').html(charX);
+					if (currentKey == currentKeyCheck) moveChar(dir);
+				});
+			}
+			// if ($('#guildo').position().top > 0) {
+			// 	$('#guildo').animate({top: '-=32'}, charSpeed, "linear", function() {
+			// 		if (currentKey == currentKeyCheck) moveChar(dir);
+			// 	});
+			// }
 			break;
+
+		case 'left':
+			if (chkMove(dir) == 'mapa') {
+				$('#mapa').animate({left: '+=32'}, charSpeed, "linear", function() {
+					charX--;
+					$('#top').html(charY);
+					$('#left').html(charX);
+					if (currentKey == currentKeyCheck) moveChar(dir);
+				});
+			}
+			else if (chkMove(dir) == 'guildo') {
+				$('#guildo').animate({left: '-=32'}, charSpeed, "linear", function() {
+					charX--;
+					$('#top').html(charY);
+					$('#left').html(charX);
+					if (currentKey == currentKeyCheck) moveChar(dir);
+				});
+			}
+			// if ($('#guildo').position().left > 0) {
+			// 	$('#guildo').animate({left: '-=32'}, charSpeed, "linear", function() {
+			// 		if (currentKey == currentKeyCheck) moveChar(dir);
+			// 	});
+			// }
+			break;
+
+		case'right':
+			if (chkMove(dir) == 'mapa') {
+				$('#mapa').animate({left: '-=32'}, charSpeed, "linear", function() {
+					charX++;
+					$('#top').html(charY);
+					$('#left').html(charX);
+					if (currentKey == currentKeyCheck) moveChar(dir);
+				});
+			}
+			else if (chkMove(dir) == 'guildo') {
+				$('#guildo').animate({left: '+=32'}, charSpeed, "linear", function() {
+					charX++;
+					$('#top').html(charY);
+					$('#left').html(charX);
+					if (currentKey == currentKeyCheck) moveChar(dir);
+				});
+			}
+			// $('#guildo').animate({left: '+=32'}, charSpeed, "linear", function() {
+			// 	if (currentKey == currentKeyCheck) moveChar(dir);
+			// });
+			break;
+
 	}
 
-	pos.x = Math.max(0, Math.min(iw - cw,pos.x));
+}// end function moveChar()
 
-});
+function chkMove(dir) {
 
-window.requestAnimationFrame(draw);
+    switch(dir) {
+
+	  case'front':
+		// if the character is greater than or equal to 64px from the bottom of the stage
+		// AND the character is not less than or equal to 64px from the top of the stage
+		// AND the "map" graphic is not at the bottom
+		if ($('#guildo').position().top <= (stageHeight - 64) 
+		    && $('#guildo').position().top >= 64
+		    && (Math.abs($('#mapa').position().top) + stageHeight) < mapHeight) {
+	  		return 'mapa';
+		} else if ($('#guildo').position().top + 32 < stageHeight) { 
+	  		return 'guildo';
+		} else {
+	  		return false;
+		}
+	    break;
+
+	  case'back':
+		// if the character is greater than or equal to 64px from the bottom of the stage
+		// AND the character is not less than or equal to 64px from the top of the stage
+		// AND the "map" graphic is not at it's top
+		if ($('#guildo').position().top >= 64 
+	      && $('#guildo').position().top < (stageHeight - 64)
+		  && $('#mapa').position().top < 0) {
+			return 'mapa';
+		} else if ($('#guildo').position().top - 32 >= 0) { 
+			return 'guildo';
+		} else {
+			return false;
+		}
+	    break;
+
+	  case'left':
+	    // if the character is greater than or equal to 64px from the left of the stage
+		// AND the character is not less than or equal to 64px from the right of the stage
+		// AND the "map" graphic is not at the right
+		if ($('#guildo').position().left < (stageWidth - 64) 
+		    && $('#guildo').position().left >= 64
+		    && $('#mapa').position().left < 0) {
+      		return 'mapa';
+		} else if ($('#guildo').position().left - 32 >= 0) { 
+			return 'guildo';
+		} else {
+			return false;
+		}
+	    break;
+
+	  case'right':
+	    // if the character is greater than or equal to 64px from the left of the stage
+		// AND the character is not less than or equal to 64px from the right of the stage
+		// AND the "map" graphic is not at the left
+		if ($('#guildo').position().left <= (stageWidth - 64) 
+		    && $('#guildo').position().left >= 64
+		    && (Math.abs($('#mapa').position().left) + stageWidth) < mapWidth) {
+			return 'map';
+		} else if ($('#guildo').position().left + 32 < stageWidth) { 
+			return 'guildo';
+		} else {
+  			return false;
+		}
+	    break;
+
+	}
+
+}// end function chkMove()
