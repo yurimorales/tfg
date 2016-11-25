8708343fc9f1b83414df672618fe3d93b8d44c88
Yuri Morales <yurimorales19@gmail.com>
2016-11-24 23:54:14 -0200

    refatoracao dos movimentos conforme os blocos do blockly informados

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,254 +1,248 @@
-var currentKey; // Armazena opção atual de comando informado(via teclado "teclas direcionais")
-var charStep = 2; //1=1st foot, 2=stand, 3=2nd foot, 4=stand
-var charSpeed = 400; //Velocidade de caminhada do personage,
-
-//[x,y], posicao inicial do personagem no cenario
+//Position x e y do canvas
 var charX = localStorage.getItem('charX'); //1 unit=32px
 var charY = localStorage.getItem('charY'); //1 unit=32px
 
+var charStep = 2; //1=1st foot, 2=stand, 3=2nd foot, 4=stand
+var charSpeed = 400; //Velocidade de caminhada do personage,
+
+//Variaveis globais do jogo
 var stageWidth = 640;
 var stageHeight = 480;
 var mapWidth = 800;//largura mapa
 var mapHeight = 800;//altura do mapa
 
-//Funçao movimenta o personagem
-function moveChar(dir) {
-
-	//Armazena key inicial de animacao
-	var currentKeyCheck = currentKey;
+//Load dos commandos do blockly
+var workspace = Blockly.inject('DivBlockly',{ toolbox: document.getElementById('toolbox') });
+Blockly.JavaScript.addReservedWords('code','move_top','move_bottom','move_right','move_left');
 	
-	//ajuste por parte da position up/down, via codigo
-	if (dir == 'up') dir = 'back';
-	if (dir == 'down') dir = 'front';
-
-	charStep++;
-	if (charStep == 5) charStep = 1;
+	Blockly.Blocks['move_top'] = {
+		init: function() {
+			this.appendDummyInput()
+				.appendField("mover para cima");
+			this.setInputsInline(false);
+			this.setPreviousStatement(true, null);
+			this.setNextStatement(true, null);
+			this.setColour(290);
+			this.setTooltip('');
+	  	}
+	}
 	
-	//Remove classe atual do personagem
-	$('#guildo').removeAttr('class');
-	
-	console.log("charStep: "+charStep);
-
-	//Adiciona nova classe
-	switch(charStep) {
-			
-		case 1: 
-			$('#guildo').addClass(dir+'-stand'); 
-			
-			setTimeout(function() {
-				charStep++;
-				if (charStep == 5) charStep = 1;
-				$('#guildo').removeAttr('class');
-				$('#guildo').addClass(dir+'-right'); 
-			}, (charSpeed/3));
-			
-			setTimeout(function() { 
-				charStep++;
-				if (charStep == 5) charStep = 1;
-				$('#guildo').removeAttr('class');
-				$('#guildo').addClass(dir+'-stand'); 
-			}, ((charSpeed/3)*2));
-			break;
-
-		case 2: 
-			$('#guildo').addClass(dir+'-right');
-
-			setTimeout(function() { 
-				charStep++;
-				if (charStep == 5) charStep = 1;
-				$('#guildo').removeAttr('class');
-				$('#guildo').addClass(dir+'-stand'); 
-			}, (charSpeed/3));
-
-			setTimeout(function() { 
-				charStep++;
-				if (charStep == 5) charStep = 1;
-				$('#guildo').removeAttr('class');
-				$('#guildo').addClass(dir+'-left'); 
-			}, ((charSpeed/3)*2));
-			break;
-
-		case 3: 
-	      	$('#guildo').addClass(dir+'-stand');
-
-			setTimeout(function() { 
-				charStep++;
-				if (charStep == 5) charStep = 1;
-				$('#guildo').removeAttr('class');
-				$('#guildo').addClass(dir+'-left'); 
-			}, (charSpeed/3));
-
-			setTimeout(function() { 
-				charStep++;
-				if (charStep == 5) charStep = 1;
-				$('#guildo').removeAttr('class');
-				$('#guildo').addClass(dir+'-stand'); 
-				}, ((charSpeed/3)*2)); 
-	    	break;
-
-		case 4: 
-			$('#guildo').addClass(dir+'-left');
-
-			setTimeout(function() { 
-				charStep++;
-				if (charStep == 5) charStep = 1;
-				$('#guildo').removeAttr('class');
-				$('#guildo').addClass(dir+'-stand'); 
-				}, (charSpeed/3));
-
-			setTimeout(function() { 
-				charStep++;
-				if (charStep == 5) charStep = 1;
-					$('#guildo').removeAttr('class');
-					$('#guildo').addClass(dir+'-right'); 
-				}, ((charSpeed/3)*2));
-			break;
+	Blockly.Blocks['move_bottom'] = {
+		init: function() {
+			this.appendDummyInput()
+				.appendField("mover para baixo");
+			this.setInputsInline(false);
+			this.setPreviousStatement(true, null);
+			this.setNextStatement(true, null);
+			this.setColour(290);
+			this.setTooltip('');
+	  	}
 	}
 
-	//currentKey = currentKey+1;
 
-    console.log("currentKey: "+currentKey);
-    console.log("currentKeyCheck: "+currentKeyCheck);
+	Blockly.Blocks['move_right'] = {
+		init: function() {
+			this.appendDummyInput()
+				.appendField("mover para a direita");
+			this.setInputsInline(false);
+			this.setPreviousStatement(true, null);
+			this.setNextStatement(true, null);
+			this.setColour(290);
+			this.setTooltip('');
+	  	}
+	}
 
-	//Move personagem conforme direcao informada pela variavel dir
-	switch(dir) {
+	Blockly.Blocks['move_left'] = {
+		init: function() {
+			this.appendDummyInput()
+				.appendField("mover para a esquerda");
+			this.setInputsInline(false);
+			this.setPreviousStatement(true, null);
+			this.setNextStatement(true, null);
+			this.setColour(290);
+			this.setTooltip('');
+	  	}
+	}
+
+
+	Blockly.JavaScript['move_top'] = function(block) {
+
+		$('#guildo').removeAttr('class').addClass('back-stand');
+
+		if (chkMove('back') == 'mapa') {
+			var codeMoveTop = "$(\"#mapa\").animate({\n top: \"+=32\"}, "+charSpeed+", \"linear\", function() {\n "+(charY--)+"; });";
+
+		}else if (chkMove('back') == 'guildo') {
+			var codeMoveTop = "$(\"#guildo\").animate({\n top: \"-=32\"}, "+charSpeed+", \"linear\", function() {\n "+(charY--)+"; });"
+		}
+
+		return codeMoveTop;
 		
-	    case 'front':
-		    if (chkMove(dir) == 'mapa') {
-		  		$('#mapa').animate({top: '-=32'}, charSpeed, "linear", function() {
-					charY++;
-					console.log("front1: "+charX+","+charY);
-		    		if (currentKey == currentKeyCheck) moveChar(dir);
-			  	});
-			} else if (chkMove(dir) == 'guildo') {
-		  		$('#guildo').animate({top: '+=32'}, charSpeed, "linear", function() {
-		  			charY++;
-					console.log("front2: "+charX+","+charY);
-			    	if (currentKey == currentKeyCheck) moveChar(dir);
-			  	});
-			}
-			break;
+	};
+
+
+	Blockly.JavaScript['move_bottom'] = function(block) {
+		
+		$('#guildo').removeAttr('class').addClass('front-stand');
+
+		if (chkMove('front') == 'mapa') {
+			var codeMoveBottom = "$(\"#mapa\").animate({\n top: \"-=32\"}, "+charSpeed+", \"linear\", function() {\n "+(charY++)+"; });";
+
+		}else if (chkMove('front') == 'guildo') {
+			var codeMoveBottom = "$(\"#guildo\").animate({\n top: \"+=32\"}, "+charSpeed+", \"linear\", function() {\n "+(charY++)+"; });"
+		}
+
+		return codeMoveBottom;
+		
+	};
+
+
+	Blockly.JavaScript['move_left'] = function(block) {
+		
+		$('#guildo').removeAttr('class').addClass('left-stand');
+
+		if (chkMove('left') == 'mapa') {
+			var codeMoveLeft = "$(\"#mapa\").animate({\n left: \"+=32\"}, "+charSpeed+", \"linear\", function() {\n "+(charX--)+"; });";
+
+		}else if (chkMove('left') == 'guildo') {
+			var codeMoveLeft = "$(\"#guildo\").animate({\n left: \"-=32\"}, "+charSpeed+", \"linear\", function() {\n "+(charX--)+"; });"
+		}
+
+		return codeMoveLeft;
+		
+	};
+
+	Blockly.JavaScript['move_right'] = function(block) {
+		
+		$('#guildo').removeAttr('class').addClass('right-stand');
+
+		if (chkMove('right') == 'mapa') {
+
+			var codeMoveRight = "$(\"#mapa\").animate({\n left: \"-=32\"}, "+charSpeed+", \"linear\", function() {\n "+(charX++)+"; });";
+
+		}else if (chkMove('right') == 'guildo') {
+			var codeMoveLeft = "$(\"#guildo\").animate({\n left: \"+=32\"}, "+charSpeed+", \"linear\", function() {\n "+(charX++)+"; });"
+		}
+
+		return codeMoveRight;
+
+	};
+
+ 	//http://stackoverflow.com/questions/31988318/google-blockly-jsinterpreter-and-interpreter-step-code
+	//http://stackoverflow.com/questions/36499757/how-can-i-make-an-instance-of-a-block-of-blockly-with-javascript
+	//http://stackoverflow.com/questions/20129236/creating-functions-dynamically-in-js
+	//https://developers.google.com/blockly/guides/app-integration/running-javascript
+	function showCode(){
+		Blockly.JavaScript.INFINITE_LOOP_TRAP = null;
+		var code = Blockly.JavaScript.workspaceToCode(workspace);
+		alert(code);
+	}
+
+	function runCode(){
+		window.LoopTrap = 1000;
+		Blockly.JavaScript.INFINITE_LOOP_TRAP = 'if (--window.LoopTrap == 0) throw "Infinite loop.";\n';
+		
+		var code = Blockly.JavaScript.workspaceToCode(workspace);
+		Blockly.JavaScript.INFINITE_LOOP_TRAP = null;
+		try {
 			
-		case 'back':
-			if (chkMove(dir) == 'mapa') {
-				$('#mapa').animate({top: '+=32'}, charSpeed, "linear", function() {
-					charY--;
-					console.log("back1: "+charX+","+charY);
-					if (currentKey == currentKeyCheck) moveChar(dir);
-				});
-			}else if (chkMove(dir) == 'guildo') {
-				$('#guildo').animate({top: '-=32'}, charSpeed, "linear", function() {
-					charY--;
-					console.log("back2: "+charX+","+charY);
-					if (currentKey == currentKeyCheck) moveChar(dir);
-				});
-			}
-			break;
+			eval(code);
+			
+		} catch (e) {
+			alert('Erro:'+e);
+		}
+	}
+	
+	//Restaurando valor inicial das variaveis setadas no localStorage
+	function updateAllValues() {
+		
+		if(!localStorage.getItem('pontuacao')){
+			localStorage.setItem('pontuacao', 0);
+		}
 
-		case 'left':
-			if (chkMove(dir) == 'mapa') {
-				$('#mapa').animate({left: '+=32'}, charSpeed, "linear", function() {
-					charX--;
-					console.log("left1: "+charX+","+charY);
-					if (currentKey == currentKeyCheck) moveChar(dir);
-				});
-			}
-			else if (chkMove(dir) == 'guildo') {
-				$('#guildo').animate({left: '-=32'}, charSpeed, "linear", function() {
-					charX--;
-					console.log("left2: "+charX+","+charY);
-					if (currentKey == currentKeyCheck) moveChar(dir);
-				});
-			}
-			break;
+		if(!localStorage.getItem('percentual_vida')){
+			localStorage.setItem('percentual_vida', 100);
+		}
 
-		case'right':
-			if (chkMove(dir) == 'mapa') {
-				$('#mapa').animate({left: '-=32'}, charSpeed, "linear", function() {
-					charX++;
-					console.log("right1: "+charX+","+charY);
-					if (currentKey == currentKeyCheck) moveChar(dir);
-				});
-			}
-			else if (chkMove(dir) == 'guildo') {
-				$('#guildo').animate({left: '+=32'}, charSpeed, "linear", function() {
-					charX++;
-					console.log("right2: "+charX+","+charY);
-					if (currentKey == currentKeyCheck) moveChar(dir);
-				});
-			}
-			break;
+		if(!localStorage.getItem('charX')){
+			localStorage.setItem('charX', 4);
+		}
+
+		if(!localStorage.getItem('charY')){
+			localStorage.setItem('charY', 6);
+		}
+
+		$("#pontuacao-atual").html( localStorage.getItem('pontuacao') );
+		$("#barra-vida").css("width", localStorage.getItem('percentual_vida')+"%");
 
 	}
 
-}// fim da function moveChar()
+	function chkMove(dir) {
 
-function chkMove(dir) {
+	    switch(dir) {
 
-    switch(dir) {
+		  case 'front':
+		  case 'down':
+			//Se o personagem é maior do que ou igual a 64px a partir do fundo do mapa
+			//e o personagem não é menos do que ou igual a 64px a partir do topo da fase
+			//e o "mapa" gráfico não é na parte inferior
+			if ($('#guildo').position().top <= (stageHeight - 64) 
+			    && $('#guildo').position().top >= 64
+			    && (Math.abs($('#mapa').position().top) + stageHeight) < mapHeight) {
+		  		return 'mapa';
+			} else if ($('#guildo').position().top + 32 < stageHeight) { 
+		  		return 'guildo';
+			} else {
+		  		return false;
+			}
+		    break;
 
-	  case 'front':
-	  case 'down':
-		//Se o personagem é maior do que ou igual a 64px a partir do fundo do mapa
-		//e o personagem não é menos do que ou igual a 64px a partir do topo da fase
-		//e o "mapa" gráfico não é na parte inferior
-		if ($('#guildo').position().top <= (stageHeight - 64) 
-		    && $('#guildo').position().top >= 64
-		    && (Math.abs($('#mapa').position().top) + stageHeight) < mapHeight) {
-	  		return 'mapa';
-		} else if ($('#guildo').position().top + 32 < stageHeight) { 
-	  		return 'guildo';
-		} else {
-	  		return false;
+		  case 'back':
+		  case 'up':
+			//Se o personagem é maior do que ou igual a 64px a partir do fundo do mapa
+			//e o personagem não é menos do que ou igual a 64px a partir do topo da fase
+			//e o "mapa" gráfico não é na parte superior
+			if ($('#guildo').position().top >= 64 
+		      && $('#guildo').position().top < (stageHeight - 64)
+			  && $('#mapa').position().top < 0) {
+				return 'mapa';
+			} else if ($('#guildo').position().top - 32 >= 0) { 
+				return 'guildo';
+			} else {
+				return false;
+			}
+		    break;
+
+		  case 'left':
+		    //Se o personagem é maior do que ou igual a 64px a partir da esquerda da fase
+			//e o personagem não é menos do que ou igual a 64px da direita da fase
+			//e o "mapa" gráfico não é à direita
+			if ($('#guildo').position().left < (stageWidth - 64) 
+			    && $('#guildo').position().left >= 64
+			    && $('#mapa').position().left < 0) {
+	      		return 'mapa';
+			} else if ($('#guildo').position().left - 32 >= 0) { 
+				return 'guildo';
+			} else {
+				return false;
+			}
+		    break;
+
+		  case 'right':
+		    //Se o carácter é maior do que ou igual a 64px a partir da esquerda da fase
+			//e o carácter não é menos do que ou igual a 64px da direita da fase
+			//e o "mapa" gráfico não está à esquerda
+			if ($('#guildo').position().left <= (stageWidth - 64) 
+			    && $('#guildo').position().left >= 64
+			    && (Math.abs($('#mapa').position().left) + stageWidth) < mapWidth) {
+				return 'mapa';
+			} else if ($('#guildo').position().left + 32 < stageWidth) { 
+				return 'guildo';
+			} else {
+	  			return false;
+			}
+		    break;
+
 		}
-	    break;
 
-	  case 'back':
-	  case 'up':
-		//Se o personagem é maior do que ou igual a 64px a partir do fundo do mapa
-		//e o personagem não é menos do que ou igual a 64px a partir do topo da fase
-		//e o "mapa" gráfico não é na parte superior
-		if ($('#guildo').position().top >= 64 
-	      && $('#guildo').position().top < (stageHeight - 64)
-		  && $('#mapa').position().top < 0) {
-			return 'mapa';
-		} else if ($('#guildo').position().top - 32 >= 0) { 
-			return 'guildo';
-		} else {
-			return false;
-		}
-	    break;
-
-	  case 'left':
-	    //Se o personagem é maior do que ou igual a 64px a partir da esquerda da fase
-		//e o personagem não é menos do que ou igual a 64px da direita da fase
-		//e o "mapa" gráfico não é à direita
-		if ($('#guildo').position().left < (stageWidth - 64) 
-		    && $('#guildo').position().left >= 64
-		    && $('#mapa').position().left < 0) {
-      		return 'mapa';
-		} else if ($('#guildo').position().left - 32 >= 0) { 
-			return 'guildo';
-		} else {
-			return false;
-		}
-	    break;
-
-	  case 'right':
-	    //Se o carácter é maior do que ou igual a 64px a partir da esquerda da fase
-		//e o carácter não é menos do que ou igual a 64px da direita da fase
-		//e o "mapa" gráfico não está à esquerda
-		if ($('#guildo').position().left <= (stageWidth - 64) 
-		    && $('#guildo').position().left >= 64
-		    && (Math.abs($('#mapa').position().left) + stageWidth) < mapWidth) {
-			return 'mapa';
-		} else if ($('#guildo').position().left + 32 < stageWidth) { 
-			return 'guildo';
-		} else {
-  			return false;
-		}
-	    break;
-
-	}
-
-}// fim da function chkMove()
+	}// fim da function chkMove
